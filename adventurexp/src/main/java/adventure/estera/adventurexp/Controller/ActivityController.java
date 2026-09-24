2ee140786064a415b08bb10f046c7a9b8397dd9a
AdamLevy2001 <adle0001@stud.ek.dk>
2026-09-24 11:28:23 +0200

    Controller

--- adventurexp/src/main/java/adventure/estera/adventurexp/Controller/ActivityController.java
+++ adventurexp/src/main/java/adventure/estera/adventurexp/Controller/ActivityController.java
@@ -1,11 +1,31 @@
 package adventure.estera.adventurexp.Controller;
 
+import adventure.estera.adventurexp.model.Activity;
+import adventure.estera.adventurexp.service.ActivityService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/adventure/activity")
 public class ActivityController {
 
+    private final ActivityService activityService;
 
+    public ActivityController(ActivityService activityService) {
+        this.activityService = activityService;
+    }
+
+    @GetMapping
+    public List<Activity> getAllActivitys() {
+        return activityService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Activity getActivty(@PathVariable Long id) {
+        return activityService.getOne(id);
+    }
 }
